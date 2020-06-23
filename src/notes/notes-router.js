8a0510fc13c2609fe1dd8546c6525d2b890ad551
Kelleymb <kelleybuccieri@gmail.com>
2020-06-23 16:10:27 -0400

    routers created

---
+++ src/notes/notes-router.js
@@ -0,0 +1,81 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const NotesService = require('./notes/notes-service')
+
+const notesRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeNote = note => ({
+    id: note.id,
+    name: xss(note.name),
+    content: xss(note.content),
+    modified: note.modified,
+    created: note.created,
+    folder_id: note.folder_id
+})
+
+notesRouter
+    .route('/')
+    .get((req, res, next) => {
+        const knexInstance = req.app.get('db')
+        NotesService.getAllNotes(knexInstance)
+            .then(notes => {
+                res.json(notes.map(serializeNote))
+            })
+            .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+        const { name, content } = req.body
+        const newNote = { name, content }
+
+        for (const [key, value] of Object.entries(newNote))
+            if (value == null)
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body` }
+                })
+        ArticlesService.insertNote(
+            req.app.get('db'),
+            newNote
+        )            
+            .then(note => {
+                res
+                    .status(201)
+                    .location(path.posix.join(req.originalUrl, + `/${note.id}`))
+                    .json(serializeNote(note))
+            })
+            .catch(next)
+    })
+
+notesRouter
+    .route('/:note_id')
+    .all((req, res, next) => {
+        NotesService.getById(
+            req.app.get('db'),
+            req.params.id
+        )
+        .then(note => {
+            if (!note) {
+                return res.status(404).json({
+                    error: { message: `Note does not exist` }
+                })
+            }
+        })
+        .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(serializeNote(res.note))
+    })
+    .delete((req, res, next) => {
+        NotesService.deleteNote(
+            req,app.get('db'),
+            req.params.id
+        )
+        .then(numRowsAffected => {
+            res.status(204).end()
+        })
+        .catch(next)
+    })
+
+
+module.exports = notesRouter
