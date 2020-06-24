7744f904cce2f098624f30157441d68d6f545188
Kelleymb <kelleybuccieri@gmail.com>
2020-06-24 12:50:19 -0400

    ran migration and seed files

--- seeds/seed.notes.sql
+++ seeds/seed.notes.sql
@@ -1,6 +1,6 @@
 INSERT INTO notes ("name", content, folder_id)
 VALUES
-  ('Note One', 'You\'ve got a rock!', 1),
-  ('Note Two', 'You\'ve got paper!', 2),
-  ('Note Three', 'You\'ve got scissors!', 3)
+  ('Note One', 'You''ve got a rock!', 1),
+  ('Note Two', 'You''ve got paper!', 2),
+  ('Note Three', 'You''ve got scissors!', 3)
 ;
