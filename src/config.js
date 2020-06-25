8d819180338ea0c557d80f7e347a1e6976abcab9
Kelleymb <kelleybuccieri@gmail.com>
2020-06-24 20:57:18 -0400

    server deploy updates

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/noteful',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
+    // DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/noteful',
+    // TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
     //deploy prep
-    // DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
-    // TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
   }
