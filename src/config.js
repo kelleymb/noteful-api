1daa0d940bcf02401d315f380ebdfa3fb67edd19
Kelleymb <kelleybuccieri@gmail.com>
2020-06-24 20:00:10 -0400

    code updates

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ module.exports = {
     API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
     DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/noteful',
     TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
+    //deploy prep
     // DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
     // TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
   }
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const { PORT } = require('./config')
 // const { PORT, DATABASE_URL } = require('./config')
 const knex = require('knex')
 
+//deploy prep
 // const db = knex({
 //   client: 'pg',
 //   connection: DATABASE_URL
